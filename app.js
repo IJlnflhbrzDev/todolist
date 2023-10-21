3c0ed6f1030fcb02d102f14f7742cf912e11ea89
IJlal Naufal Hibrizi <IJlnflhbrzDev@gmail.com>
2023-10-21 17:41:55 +0700

    Handle user kalo klik button add todo tapi kosong

--- app.js
+++ app.js
@@ -8,32 +8,37 @@ const todoList = document.getElementById('todo-list');
 todoForm.addEventListener('submit', addTodo);
 // membuat event click pada list todo yg dibuat oleh user ketika user klik tombol delete
 todoList.addEventListener("click", deleteTodo);
+
 function addTodo(e) {
     e.preventDefault();
-    console.log(todoInput.value);
 
-    // membuat element li untuk menampung value dari todo input 
-    const li = document.createElement('li');
-    // Menambahkan Banyak class ke dalam li
-    li.className= 'list-group-item d-flex justify-content-between align-items-center mb-1';
-    // isi content li dari value todoInput mengunakan documen create textnode
-    li.appendChild(document.createTextNode(todoInput.value));
+    if (todoInput.value) {
+
+        // membuat element li untuk menampung value dari todo input 
+        const li = document.createElement('li');
+        // Menambahkan Banyak class ke dalam li
+        li.className= 'list-group-item d-flex justify-content-between align-items-center mb-1';
+        // isi content li dari value todoInput mengunakan documen create textnode
+        li.appendChild(document.createTextNode(todoInput.value));
+        
     
-
-     // membuat tombolDelete masing masing value dari li 
-     const deleteList = document.createElement('a');
-     deleteList.href = '#'
-     deleteList.className = 'badge badge-danger delete-todo';
-     deleteList.textContent = 'Delete';
-
-     // MENGABUNGKAN ELEMEN A DI DALAM LI
-     li.appendChild(deleteList);
-
-     // MENGABUNGKAN LI KEDALAM LIST GROUP 
-    todoList.appendChild(li);
-
-    // menghapus inputan user ketika list sudah di tambahkan 
-    todoInput.value = '';
+         // membuat tombolDelete masing masing value dari li 
+         const deleteList = document.createElement('a');
+         deleteList.href = '#'
+         deleteList.className = 'badge badge-danger delete-todo';
+         deleteList.textContent = 'Delete';
+    
+         // MENGABUNGKAN ELEMEN A DI DALAM LI
+         li.appendChild(deleteList);
+    
+         // MENGABUNGKAN LI KEDALAM LIST GROUP 
+        todoList.appendChild(li);
+    
+        // menghapus inputan user ketika list sudah di tambahkan 
+        todoInput.value = '';
+    }else{
+        alert('todolist gaboleh kosong!!')
+    }
 
 
     // todoList.innerHTML = `
