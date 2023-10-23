92a38e4079c720733b689c507f143f8b133d972e
IJlal Naufal Hibrizi <IJlnflhbrzDev@gmail.com>
2023-10-23 13:40:32 +0700

    memperbaiki sedikit error

--- app.js
+++ app.js
@@ -25,30 +25,30 @@ function addTodo(e) {
         // membuat element li untuk menampung value dari todo input 
         const li = document.createElement('li');
         // Menambahkan Banyak class ke dalam li
-        li.className= 'todo-item list-group-item d-flex justify-content-between align-items-center mb-1';
+        li.className = 'todo-item list-group-item d-flex justify-content-between align-items-center mb-1';
         // isi content li dari value todoInput mengunakan documen create textnode
         li.appendChild(document.createTextNode(todoInput.value));
-        
-    
-         // membuat tombolDelete masing masing value dari li 
-         const deleteList = document.createElement('a');
-         deleteList.href = '#'
-         deleteList.className = 'badge badge-danger delete-todo';
-         deleteList.textContent = 'Delete';
-    
-         // MENGABUNGKAN ELEMEN A DI DALAM LI
-         li.appendChild(deleteList);
-    
-         // MENGABUNGKAN LI KEDALAM LIST GROUP 
+
+
+        // membuat tombolDelete masing masing value dari li 
+        const deleteList = document.createElement('a');
+        deleteList.href = '#'
+        deleteList.className = 'badge badge-danger delete-todo';
+        deleteList.textContent = 'Delete';
+
+        // MENGABUNGKAN ELEMEN A DI DALAM LI
+        li.appendChild(deleteList);
+
+        // MENGABUNGKAN LI KEDALAM LIST GROUP 
         todoList.appendChild(li);
-    
+
         // memangil function untuk set data per masing masing todolist ke localeStorage
-        // addTodoToLocaleStorage(todoInput.value);
+        addTodoToLocaleStorage(todoInput.value);
 
         // menghapus inputan user ketika list sudah di tambahkan 
         todoInput.value = '';
 
-    }else{
+    } else {
         alert('todolist gaboleh kosong!!')
     }
 
@@ -56,22 +56,23 @@ function addTodo(e) {
 
 // membuat function untuk masukan data todolist ke localeStorage
 function addTodoToLocaleStorage(todoInputValue) {
-   // membuat variabel kosong untuk menampung array todos
-   let todos;
-   // membuat kondisi jika locale storage kosong maka variabel todos di ubah menjadi array kosong
-   if(localStorage.getItem('todos' == null)){
-    todos = [''];
-   }else {
-    // jika tidak kosong maka parsing json todos 
-    todos = JSON.parse(localStorage.getItem('todos'));
-   }
+    // membuat variabel kosong untuk menampung array todos
+    let todos;
+    // membuat kondisi jika locale storage kosong maka variabel todos di ubah menjadi array kosong
+    if (localStorage.getItem('todos') == null) {
+        todos = [];
+    } else {
+        // jika tidak kosong maka parsing json todos 
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
 
-   // push setiap todos yg di buat user ke variabel todos 
-   todos.push(todoInputValue);
-   // setelah push ke variabel todos 
-   // maka set ke localeStorage, dan jangan lupa data localeStorage harus strng 
-   // maka dari itu convert terlebih dahulu datanya
-   localStorage.setItem('todos', JSON.stringify(todos));
+    // push setiap todos yg di buat user ke variabel todos 
+    todos.push(todoInputValue);
+
+    // setelah push ke variabel todos 
+    // maka set ke localeStorage, dan jangan lupa data localeStorage harus strng 
+    // maka dari itu convert terlebih dahulu datanya dari array ke string
+    localStorage.setItem('todos', JSON.stringify(todos));
 
 }
 
@@ -86,9 +87,8 @@ function deleteTodo(e) {
             const parent = e.target.parentElement;
             parent.remove();
         }
-    }else{
+    } else {
         console.log('Tidak ada delete todo');
-        console.log(e);
     }
 }
 
@@ -99,17 +99,17 @@ function clearTodos(e) {
 
 function filterTodos(e) {
     // mengambil text sesuai dengan inputan user 
-    const filterText = e.target.value; 
+    const filterText = e.target.value;
     // mengambil semua element yg memiliki class todo-item
-    const todosItem = document.querySelectorAll('.todo-item'); 
+    const todosItem = document.querySelectorAll('.todo-item');
     // todosItem ini akan melakaukan foreach masing masing element 
     todosItem.forEach((item) => {
         // mengambil text element dan melakukan convert ke huruf kecil
         let itemText = item.firstChild.textContent.toLocaleLowerCase();
         // membuat logic jika item tidak -1 artinya ada kata / daftar / list yg di cari oleh user
         if (itemText.indexOf(filterText) !== -1) {
-            item.setAttribute('style','display: block;')
-        }else {
+            item.setAttribute('style', 'display: block;')
+        } else {
             item.setAttribute('style', 'display: none !important;');
         }
     })
